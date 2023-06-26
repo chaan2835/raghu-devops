b09afe7bb73567ce39ae1eceb1d67f4ff3b6bb25
Chebrolu Chandra Sekhar <Sekhar.Chebrolu@ivycomptech.com>
2023-06-26 10:30:56 +0530

    added catalogue.sh file

---
+++ learn-shell/roboshop-shell/catalogue.sh
@@ -0,0 +1,21 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+cp catalogue.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org-shell -y
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
