5aaaf1ae176cca1edc00bfe8c3910bd41c6685d8
Chebrolu Chandra Sekhar <Sekhar.Chebrolu@ivycomptech.com>
2023-06-27 09:56:10 +0530

    added mysql.sh file

--- learn-shell/roboshop-shell/mysql.sh
+++ learn-shell/roboshop-shell/mysql.sh
@@ -4,4 +4,4 @@ yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
 mysql_secure_installation --set-root-pass RoboShop@1
-mysql -uroot -pRoboShop@1
+# mysql -uroot -pRoboShop@1
