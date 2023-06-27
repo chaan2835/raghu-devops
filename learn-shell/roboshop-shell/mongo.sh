f1e155b87f0ff45fe410399e2e5830f683c658a9
sekhar.chebrolu <sekhar.chebrolu@ivycomptech.com>
2023-06-27 19:34:41 +0530

    updated mongo.conf file with 0.0.0.0

--- learn-shell/roboshop-shell/mongo.sh
+++ learn-shell/roboshop-shell/mongo.sh
@@ -1,5 +1,5 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
-
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongo.conf
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
