355a6033c61077880928a11f62b08605a49ddcbf
sekhar.chebrolu <sekhar.chebrolu@ivycomptech.com>
2023-06-28 16:38:59 +0530

    modified rabbitmq.sh file with echo color coding

--- learn-shell/roboshop-shell/rabbitmq.sh
+++ learn-shell/roboshop-shell/rabbitmq.sh
@@ -1,8 +1,16 @@
+echo -e "\e[36m########### Setting up erlang repos #############\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
-yum install erlang -y
+
+echo -e "\e[36m########### Setting up rabbitmq repos #############\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
-yum install rabbitmq-server -y
+
+echo -e "\e[36m########### Installing erlang & rabbitmq #############\e[0m"
+yum install erlang rabbitmq-server -y
+
+echo -e "\e[36m########### Restarting service #############\e[0m"
 systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl restart rabbitmq-server
+
+echo -e "\e[36m########### Adding application user in Rabbitmq #############\e[0m"
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
