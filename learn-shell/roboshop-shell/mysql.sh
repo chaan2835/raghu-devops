727c038d04f8a0bc6f1ed9b6f65a3161872f37ec
sekhar.chebrolu <sekhar.chebrolu@ivycomptech.com>
2023-06-28 16:08:47 +0530

    modified mysql.sh file with echo color coding

--- learn-shell/roboshop-shell/mysql.sh
+++ learn-shell/roboshop-shell/mysql.sh
@@ -1,7 +1,16 @@
+echo -e "\e[36m########### Disabling mysql default version #############\e[0m"
 yum module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m########### copying msql repo file #############\e[0m"
+cp /home/centos/raghu-devops/learn-shell/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m########### installing mysql #############\e[0m"
 yum install mysql-community-server -y
+
+echo -e "\e[36m########### Restarting mysql #############\e[0m"
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
+
+echo -e "\e[36m########### Resetting mysql password #############\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
 # mysql -uroot -pRoboShop@1
