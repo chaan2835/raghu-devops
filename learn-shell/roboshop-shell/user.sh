bd8569b2b72c1aa817c735dd47af1b812ea9dacd
sekhar.chebrolu <sekhar.chebrolu@ivycomptech.com>
2023-06-28 15:38:13 +0530

    modified redis.sh file with echo color coding

--- learn-shell/roboshop-shell/user.sh
+++ learn-shell/roboshop-shell/user.sh
@@ -14,26 +14,26 @@ echo -e "\e[36m########### updating redis listen address #############\e[0m"
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
 
-echo -e "\e[36m########### updating redis listen address #############\e[0m"
+echo -e "\e[36m########### Downloading app content #############\e[0m"
 unzip /tmp/user.zip
 cd /app
 
-echo -e "\e[36m########### updating redis listen address #############\e[0m"
+echo -e "\e[36m########### Installing npm modules #############\e[0m"
 npm install
 
-echo -e "\e[36m########### updating redis listen address #############\e[0m"
+echo -e "\e[36m########### creating application directory #############\e[0m"
 cp /home/centos/raghu-devops/learn-shell/roboshop-shell/user.service /etc/systemd/system/user.service
 
-echo -e "\e[36m########### updating redis listen address #############\e[0m"
+echo -e "\e[36m########### Restarting service #############\e[0m"
 systemctl daemon-reload
 systemctl enable user
 systemctl restart user
 
-echo -e "\e[36m########### updating redis listen address #############\e[0m"
+echo -e "\e[36m########### copying mongo repo #############\e[0m"
 cp /home/centos/raghu-devops/learn-shell/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo -e "\e[36m########### updating redis listen address #############\e[0m"
+echo -e "\e[36m########### Installing mongo clinet #############\e[0m"
 yum install mongodb-org-shell -y
 
-echo -e "\e[36m########### updating redis listen address #############\e[0m"
+echo -e "\e[36m########### loading schema #############\e[0m"
 mongo --host mongodb.roboshopk8.online </app/schema/user.js
