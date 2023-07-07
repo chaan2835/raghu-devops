0359be2337385c25d4ee4ac2ed4d26dc1b18145d
sekhar.chebrolu <sekhar.chebrolu@ivycomptech.com>
2023-07-07 12:09:08 +0530

    creating and calling functions for common code

--- learn-shell/roboshop-shell/catalogue.sh
+++ learn-shell/roboshop-shell/catalogue.sh
@@ -3,43 +3,9 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-echo -e "\e[36m########### Configuring nodejs repos #############\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+component=catalogue
 
-echo -e "\e[36m########### Installing nodejs #############\e[0m"
-yum install nodejs -y
-
-echo -e "\e[36m########### Adding application user #############\e[0m"
-useradd ${app_user}
-
-echo -e "\e[36m########### Creating application dir #############\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[36m########### Downloading app content #############\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-
-echo -e "\e[36m########### Changing to app dir #############\e[0m"
-cd /app
-
-echo -e "\e[36m########### Unzipping the app content /tmp #############\e[0m"
-unzip /tmp/catalogue.zip
-cd /app
-
-echo -e "\e[36m########### Installing node modules #############\e[0m"
-npm install
-
-echo -e "\e[36m########### Coying catalogue.service to /etc #############\e[0m"
-cp ${script_path}/catalogue.service /etc/systemd/system/catalogue.service
-
-echo -e "\e[36m########### Reloading daemon #############\e[0m"
-systemctl daemon-reload
-
-echo -e "\e[36m########### Enabling the service #############\e[0m"
-systemctl enable catalogue
-
-echo -e "\e[36m########### Restarting the service #############\e[0m"
-systemctl restart catalogue
+func_nodejs
 
 echo -e "\e[36m########### Copying the mongo.repo to /etc #############\e[0m"
 cp /home/centos/raghu-devops/learn-shell/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
--- learn-shell/roboshop-shell/user.sh
+++ learn-shell/roboshop-shell/user.sh
@@ -2,36 +2,8 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-echo -e "\e[36m########### Configuring nodejs repos #############\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
-echo -e "\e[36m########### Installing nodejs #############\e[0m"
-yum install nodejs -y
-
-echo -e "\e[36m########### Adding application user #############\e[0m"
-useradd ${app_user}
-
-echo -e "\e[36m########### creating Application directory #############\e[0m"
-mkdir /app
-
-echo -e "\e[36m########### updating redis listen address #############\e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-
-echo -e "\e[36m########### Downloading app content #############\e[0m"
-unzip /tmp/user.zip
-cd /app
-
-echo -e "\e[36m########### Installing npm modules #############\e[0m"
-npm install
-
-echo -e "\e[36m########### creating application directory #############\e[0m"
-cp ${script_path}/user.service /etc/systemd/system/user.service
-
-echo -e "\e[36m########### Restarting service #############\e[0m"
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
+component=user
+func_nodejs
 
 echo -e "\e[36m########### copying mongo repo #############\e[0m"
 cp /home/centos/raghu-devops/learn-shell/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
