d31d6441a786e077741276301c9d4ad4ee56fe57
sekhar.chebrolu <sekhar.chebrolu@ivycomptech.com>
2023-07-07 11:46:22 +0530

    softcoded the passwords

--- learn-shell/roboshop-shell/payment.sh
+++ learn-shell/roboshop-shell/payment.sh
@@ -1,6 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+rabbitmq_password=$1
 
 echo -e "\e[36m########### Installing python #############\e[0m"
 yum install python36 gcc python3-devel -y
@@ -23,6 +24,7 @@ echo -e "\e[36m########### Installing dependencies #############\e[0m"
 pip3.6 install -r requirements.txt
 
 echo -e "\e[36m###########  settingup systemd service #############\e[0m"
+sed -i -e "s|rabbitmq_password|${rabbitmq_password}|" ${script_path}/payment.service
 cp ${script_path}/payment.service /etc/systemd/system/payment.service
 
 echo -e "\e[36m########### Restarting service #############\e[0m"
