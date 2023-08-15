4c615d057c50a860119579070c4c54d1c076a286
sekhar.chebrolu <sekhar.chebrolu@ivycomptech.com>
2023-08-15 18:25:30 +0530

    trying to get the aws-ami-id

--- terraform/creating-a-single-sample-ec2-instance.tf
+++ terraform/creating-a-single-sample-ec2-instance.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "centos" {
-  owners           = ["817611672488"]
+  owners           = ["973714476881"]
   most_recent      = true
   name_regex       = "Centos-8-Devops-Practice"
 }
