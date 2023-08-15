ae531ea7e4ffdb96ee181a263c629d47babe3fd8
sekhar.chebrolu <sekhar.chebrolu@ivycomptech.com>
2023-08-15 18:22:32 +0530

    trying to get the aws-ami-id

--- terraform/creating-a-single-sample-ec2-instance.tf
+++ terraform/creating-a-single-sample-ec2-instance.tf
@@ -1,3 +1,13 @@
+data "aws_ami" "aws_ami_id" {
+  most_recent      = true
+  owners           = ["817611672488"]
+  name_regex = "Centos-8-Devops-Practice"
+}
+
+output "ami_id" {
+  value = data.aws_ami.aws_ami_id.image_id
+}
+
 resource "aws_instance" "single_sample_ec2_instance" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
